b1546a5be8838beeb06938249d3bbeaa09e529c6
Brenden Dougherty <bdougherty89@yahoo.com>
2025-03-26 20:34:47 -0700

    added functionality and the game is playable but needs to be fixed for when the game is over

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
 const newGameButton = document.querySelector(".play-game-button");
 let screenContainer = document.querySelector(".screen-container");
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let player1Text = document.querySelector(".player1-p");
+let player2Text = document.querySelector(".player2-p");
+let GameCountP = document.querySelector(".game-count");
+let gameCountNum = 0;
 
 newGameButton.addEventListener("click", createUser);
 let counter = 0;
 let playerNamesArray = [];
+
 function Player(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
@@ -12,8 +19,6 @@ function Player(player1, player2) {
 function createUser() {
   newGameButton.textContent = "Play Game!";
   playerNamesArray = [];
-  console.log(newGameButton.textContent);
-  console.log(counter);
 
   if (counter === 0 && newGameButton.textContent == "Play Game!") {
     // Create modal for players to enter their name
@@ -75,7 +80,6 @@ function createUser() {
       newModal.remove();
     });
   } else {
-    console.log("this isnt working");
   }
 }
 
@@ -83,11 +87,8 @@ function updatePlayers(player1, player2) {
   counter -= 1;
   let playerNames = new Player(player1, player2);
   playerNamesArray.push(playerNames);
-  console.log(playerNamesArray);
   screenContainer.style.backgroundColor = "white";
   newGameButton.textContent = "New Game";
-  let player1Text = document.querySelector(".player1-p");
-  let player2Text = document.querySelector(".player2-p");
   player1Text.textContent = `${playerNamesArray[0].player1}: 0`;
   player2Text.textContent = `${playerNamesArray[0].player2}: 0`;
   let turn = 0;
@@ -105,25 +106,71 @@ function playNewGame() {
     createUser();
   }
 }
-
-//function for doing something with clicking shit that will make an X or an O
-
 function assignPlayerRole(player1, player2, gameDivs, turn) {
   let playerTurn = document.querySelector(".game-turn");
+  let divElements = [];
+  for (let i = 0; i < 9; i++) {
+    divElements.push({ id: gameDivs[i].textContent });
+  }
   playerTurn.textContent = `Player Turn: ${player1}`;
-  gameDivs.forEach((div) => {
+  gameDivs.forEach((div, index) => {
     div.addEventListener("click", () => {
       if (turn == 0 && div.textContent == "") {
         playerTurn.textContent = `Player Turn: ${player2}`;
+        player1Text.textContent = `${player1} Score: ${playerOneScore}`;
         div.textContent = "X";
         div.style.color = "var(--playerOneColor)";
+        divElements[index].id = div.textContent;
         turn = 1;
+        checkScore(divElements, playerTurn, player1, player2);
       } else if (turn == 1 && div.textContent == "") {
         playerTurn.textContent = `Player Turn: ${player1}`;
+        player2Text.textContent = `${player2} Score: ${playerTwoScore}`;
         div.textContent = "O";
         div.style.color = "var(--playerTwoColor)";
         turn = 0;
+        checkScore(divElements, playerTurn, player1, player2);
       }
     });
   });
+}
+
+function checkScore(divElements, playerTurn, player1, player2) {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let combo of winningCombos) {
+    let [a, b, c] = combo;
+    if (
+      divElements[a].id === "X" &&
+      divElements[b].id === "X" &&
+      divElements[c].id === "X"
+    ) {
+      playerTurn.textContent = `${player1} Wins!`;
+      playerOneScore += 1;
+      gameCountNum += 1;
+      player1Text.textContent = `${player1} Score: ${playerOneScore}`;
+      GameCountP.textContent = `Game Count: ${gameCountNum}`;
+      break;
+    } else if (
+      divElements[a].id === "O" &&
+      divElements[b].id === "O" &&
+      divElements[c].id === "O"
+    ) {
+      playerTurn.textContent = `${player2} Wins!`;
+      playerTwoScore += 1;
+      gameCountNum += 1;
+      player2Text.textContent = `${player2} Score: ${playerTwoScore}`;
+      GameCountP.textContent = `Game Count: ${gameCountNum}`;
+      break;
+    }
+  }
 }
